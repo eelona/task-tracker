bc3f47002690c0215bb1dcf63b52bc7f5aa2cb79
elonathaqi <eelonathaqi@gmail.com>
2024-10-10 09:26:44 +0200

    initialized project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+  <h1 className="text-2xl font-bold py-4 pl-6">Task Tracker</h1>
+  <p className="text-xl pl-6">Click +New to add a task</p>
+</>
   );
 }
 
